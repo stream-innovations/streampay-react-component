d04d659268d1e10019fc4fd49ca4218444aa939f
StreamINNOVATIONS <133185170+stream-innovations@users.noreply.github.com>
2023-07-18 16:24:38 +0300

    Update StreamPayButton.js
    Added button component

---
+++ src/components/Checkout/StreamPayButton.js
@@ -0,0 +1,9 @@
+// StreamPayButton.js
+import React from 'react';
+import './css/checkout.css';
+
+const StreamPayButton = () => {
+  // Component logic and rendering
+}
+
+export default StreamPayButton;
